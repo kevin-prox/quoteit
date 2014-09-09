0246d1bdd7e14309da5773a9537c84caa5dffe81
Kevin Prox <Kevin.Prox@wnco.com>
2014-09-09 13:53:16 -0500

    Kevin Prox: Added confirmation mail functions

--- routes/confirmation.js
+++ routes/confirmation.js
@@ -9,23 +9,23 @@ router.get('/newuser/:email/x/:code', function(req, res) {
 	
 	db.collection('users').find({'email' : req.params.email}).toArray(function(err, items) {
 		
-		$.each(items, function() {
+		/*$.each(items, function() {
 			
 			// If code sent matches code stored, then mark user as verified
 			if (this.code === req.params.code) {
 				
-				res.send({msg : 'TEST'});
-				
-				/*db.collection('users').update({ email : req.params.email }, { $set : { verif : 'Y' } }, 
+				db.collection('users').update({ email : req.params.email }, { $set : { verif : 'Y' } }, 
 					function(err, result) {
 						
 						if (err === null) {
 							
 							res.render('conf', { title: 'Quote It!' });
 						}
-				});*/
+				});
 			}
-		});
+		});*/
+		
+		res.send(items);
 	});
 });
 
